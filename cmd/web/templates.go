f439861bbb9792864285b19c3558e82da286cccb
Leonardo Henrique Romanini <leonardo.romanini@ciandt.com>
2024-12-21 20:49:54 -0300

    Partial commit

---
+++ cmd/web/templates.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"go_learning/internal/models"
+	"html/template"
+	"path/filepath"
+)
+
+type templateData struct {
+	CurrentYear int
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		// Parse the base template file into a template set.
+		ts, err := template.ParseFiles("./ui/html/base.tmpl.html")
+		if err != nil {
+			return nil, err
+		}
+
+		// Call ParseGlob() *on this template set* to add any partials.
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
+		if err != nil {
+			return nil, err
+		}
+
+		// Call ParseFiles() *on this template set* to add the  page template.
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		// Add the template set to the map as normal...
+		cache[name] = ts
+	}
+
+	return cache, nil
+}
