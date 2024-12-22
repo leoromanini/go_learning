85003aa3dd003b60c3ba1949e4cc7588ada4a2df
Leonardo Henrique Romanini <leonardo.romanini@ciandt.com>
2024-12-22 18:24:16 -0300

    Addind middleware

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,6 +10,7 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
+
 	if r.URL.Path != "/" {
 		app.notFound(w)
 		return
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -13,5 +13,5 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 	mux.HandleFunc("/snippet/list", app.snippetList)
 
-	return mux
+	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 }
